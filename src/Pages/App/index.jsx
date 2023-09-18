74af09c6a92136cf7ebbcf6d1d78be95e7dad67c
Sandra Cordoba <sandracordoba16@gmail.com>
2023-09-17 21:12:29 -0300

    create of archive

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,13 @@
+import Home from '../Home'
+import MyAccount from '../MyAccount'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <MyAccount />
+    </>
+  )
+}
+export default App
---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,8 @@
+function Home() {
+    return (
+        <div className='flex flex-col justify-center items-center mt-8 text-lg font-bold'>
+            <h1>Home</h1>
+        </div>
+    )
+}
+export default Home
